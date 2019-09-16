f7eaa46d15e2ccdaa92dcc3977c3a695dab68ed1
Sluttmann <Sluttmann@mail.usf.edu>
2019-09-16 00:22:31 -0800

    Added Comments

--- Program.cs
+++ Program.cs
@@ -4,24 +4,43 @@ namespace Deliverable_3___Methods
 {
     class Program
     {
+        //First Method
         static void Main(string[] args)
         {
-            Console.Write("What is your first name?: ");
-            string first = Console.ReadLine();
+           try
+            {
+                // Ask user for first name
+                Console.Write("What is your first name?: ");
+                string first = Console.ReadLine();
 
-            Console.Write("What is your Last name?: ");
-            string last = Console.ReadLine();
+                // Ask user for last name
+                Console.Write("What is your Last name?: ");
+                string last = Console.ReadLine();
 
-            Console.Write("What tempature in Fahrenheit do you wnat to convert?: ");
-            double temp = double.Parse(Console.ReadLine());
-            Console.WriteLine("");
+                // Ask user what temp they want to convert
+                Console.Write("What tempature in Fahrenheit do you wnat to convert?: ");
+                double temp = double.Parse(Console.ReadLine());
+                // extra space
+                Console.WriteLine("");
 
-            Method_2(temp, first, last);
+                // Method call and push 
+                Method_2(temp, first, last);
+            }
+            catch (Exception ex)
+            {
+                // error message
+                Console.WriteLine(ex.Message);
+            }
+            
         }
+        // Second Method
         private static void Method_2(double temp, string first, string last)
         {
+            // Output of first and last name
             Console.WriteLine("Hello " + first + " " + last + "!");
+            // convert temps F to C
             double celsius = (temp - 32) * 5 / 9;
+            // Output user temp and converted temp
             Console.WriteLine("The tempature " + temp + " degrees Fahrenheit in Celsius is: " + celsius);
         }
     }
